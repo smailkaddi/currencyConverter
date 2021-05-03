2de79120a27e00a52a6b8b25a7a4402594cdb220
kaddi smail <kaddi.smail1995@gmail.com>
2021-05-03 14:12:40 +0000

    update

--- App.js
+++ App.js
@@ -1,14 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View,ImageBackground,FlatList,useState } from 'react-native';
-import FlatButton from './Button/Button';
-import Navigator from './routes/homeStack';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './components/Home'
+import CurrencyComponents from './components/currencyTable'
+
+const Stack = createStackNavigator();
 
 export default function App() {
- 
-
   return (
-      <Navigator/>
+    <NavigationContainer>
+    <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="currencyTable" component={CurrencyComponents} />
+    </Stack.Navigator>
+  </NavigationContainer>
   );
 }
-
